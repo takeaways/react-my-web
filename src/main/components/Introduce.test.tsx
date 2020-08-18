98ec79c871ed05b2383e6e3e755decede5577111
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-19 02:00:52 +0900

    UPDATE test code made
    테스트 코드 작성 추가 했습니다.

--- src/main/components/Introduce.test.tsx
+++ src/main/components/Introduce.test.tsx
@@ -5,6 +5,7 @@ import Introduce from './Introduce';
 describe('Introduce',()=>{
   it('render Introduce',()=>{
     const {getByText} = render((<Introduce/>));
-    expect(getByText(/Hello/)).not.toBeNull();
+
+    expect(getByText(/Javascript Developer/)).not.toBeNull();
   })
 })
