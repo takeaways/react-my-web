ea1e57dffd5849f3ed24e2677488c10924db7b18
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-19 00:10:36 +0900

    ADD PageContainer
    페이지를 랩핑하는 컨테이너를 만들어서 사용합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import TopBar from './common/components/TopBar';
 
 import Main from './main/containers/Main';
+import PageContainer from './common/components/PageContainer';
 
 function App() {
   return (
     <BrowserRouter>
-      <TopBar/>
-      <Switch>
-        <Route path="/" component={Main}/>
-      </Switch>
+      <TopBar />
+      <PageContainer>
+        <Switch>
+          <Route path='/' component={Main} />
+        </Switch>
+      </PageContainer>
     </BrowserRouter>
   );
 }
