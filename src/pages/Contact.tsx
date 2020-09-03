fdf91d8b77463271574dfd4d7586a1999b3013cc
JangGeonil <wkdrjsdlf2@gmail.com>
2020-09-03 21:21:36 +0900

    add pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Contact() {
+  return <div>Contact Contact</div>
+}
+
+export default Contact
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,9 @@
  */
 import React from 'react'
 
+
+
+
 const routes = [
   {
     path: '/main',
