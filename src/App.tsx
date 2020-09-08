1dfdbef246ec9c3786b2605752d97dd1091c5d7f
JangGeonil <wkdrjsdlf2@gmail.com>
2020-09-08 20:52:47 +0900

    text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Header, NavBar, Main, Footer, SideBar } from './Components/Layout'
+import { Header, NavBar, Main, Footer } from './Components/Layout'
 import MainRouter from './Routes'
 
 function App() {
-
   return (
     <>
       <Header />
