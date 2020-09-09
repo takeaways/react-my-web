c78547f28241846762399dd7c7a539aee0db72e5
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-09-10 00:59:52 +0900

    flex

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -5,33 +5,50 @@ export default function Work(props: any) {
   return (
     <Container>
       <div className="flex-container">
-        <div className="flex-item">1</div>
-        <div className="flex-item">2</div>
+        <div className="flex-item">1111111</div>
+        <div className="flex-item">22222</div>
         <div className="flex-item">3</div>
+        <div className="flex-item">444444</div>
+        <div className="flex-item">22222</div>
+        <div className="flex-item">3</div>
+        <div className="flex-item">444444</div>
+        <div className="flex-item">22222</div>
+        <div className="flex-item">3</div>
+        <div className="flex-item">444444</div>
+        <div className="flex-item">22222</div>
+        <div className="flex-item">3</div>
+        <div className="flex-item">444444</div>
       </div>
     </Container>
   )
 }
 
 const Container = styled.section`
+  background-color: red;
+  height: 100%;
   .flex-container {
     display: flex;
     flex-direction: column;
-    height: 300px;
+    min-height: 100%;
   }
+
+  .flex-item {
+    flex: 1 auto;
+  }
+
   @media (min-width: 600px) {
     .flex-container {
-      display: flex;
       flex-direction: row;
-      justify-content: space-between;
-      align-items: start;
-      /* flex-wrap: wrap; */
-      /* flex-flow: row wrap; */
-      /* flex-direction: column; */
-      /* flex-wrap: nowrap; */
+      flex-wrap: wrap;
     }
-    /* .flex-item {
-      flex: 1;
-    } */
+    .flex-item {
+      flex-basis: 50%;
+    }
+  }
+
+  @media (min-width: 800px) {
+    .flex-item {
+      flex-basis: 33%;
+    }
   }
 `
