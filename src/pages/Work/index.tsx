485d14eaf323fc5e67fa786eec0fb991e6b4846e
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-09-10 00:22:10 +0900

    flex

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -14,17 +14,17 @@ export default function Work(props: any) {
 }
 
 const Container = styled.section`
-  height: 100%;
-
   .flex-container {
     display: flex;
     flex-direction: column;
+    height: 300px;
   }
   @media (min-width: 600px) {
     .flex-container {
       display: flex;
       flex-direction: row;
-      justify-content: center;
+      justify-content: space-between;
+      align-items: start;
       /* flex-wrap: wrap; */
       /* flex-flow: row wrap; */
       /* flex-direction: column; */
