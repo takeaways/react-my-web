c6a2c2533fe6349facad9bb97890b778efafdfb7
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-09-10 00:19:50 +0900

    flex

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -5,28 +5,33 @@ export default function Work(props: any) {
   return (
     <Container>
       <div className="flex-container">
-        <div className="flex-item">
-          1 Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit amet
-          consectetur, Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor
-          sit amet consectetur,
-        </div>
+        <div className="flex-item">1</div>
         <div className="flex-item">2</div>
         <div className="flex-item">3</div>
       </div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti aut
-      magnam sunt fugit nulla ut? Explicabo reprehenderit eaque dolores, beatae,
-      eius magni rem voluptatem, architecto ad earum quos tempora blanditiis?
     </Container>
   )
 }
 
 const Container = styled.section`
   height: 100%;
+
   .flex-container {
     display: flex;
-    flex-wrap: wrap;
-    /* flex-flow: row wrap; */
-    /* flex-direction: column; */
-    /* flex-wrap: nowrap; */
+    flex-direction: column;
+  }
+  @media (min-width: 600px) {
+    .flex-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      /* flex-wrap: wrap; */
+      /* flex-flow: row wrap; */
+      /* flex-direction: column; */
+      /* flex-wrap: nowrap; */
+    }
+    /* .flex-item {
+      flex: 1;
+    } */
   }
 `
