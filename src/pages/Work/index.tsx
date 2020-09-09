45fe7b931ca052e6f985a3c28debec8e82b8228e
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-09-10 00:06:39 +0900

    flex

--- src/pages/Work/index.tsx
+++ src/pages/Work/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
-function Work(props: any) {
-  return <div>Work Work</div>
+import styled from 'styled-components'
+import './index.css'
+export default function Work(props: any) {
+  return (
+    <Container>
+      <div className="flex-container">
+        <div className="flex-item">1</div>
+        <div className="flex-item">2</div>
+        <div className="flex-item">3</div>
+      </div>
+    </Container>
+  )
 }
 
-export default Work
+const Container = styled.section`
+  height: 100%;
+  .flex-container {
+    display: flex;
+  }
+`
