0460a92e97595f352ef8d8e14cbf98c7d7017990
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-10-01 05:30:20 +0900

    video search

--- src/pages/Skill/components/search_header/search_header.tsx
+++ src/pages/Skill/components/search_header/search_header.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './search_header.module.css'
-export default function SearchHeader() {
+export default function SearchHeader({ onSearch }: any) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const handleSearch = () => {
+    const value = inputRef.current!.value
+    onSearch(value)
+  }
+  const onClick = () => {
+    handleSearch()
+  }
+  const onKeyPress = (e: any) => {
+    if (e.key === 'Enter') handleSearch()
+  }
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img className={styles.img} src="/images/logo.png" alt="logo" />
         <h1 className={styles.title}>VideoBe</h1>
       </div>
-      <input className={styles.input} type="search" placeholder="Search...." />
-      <button className={styles.button} type="submit">
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search...."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
         <img
           className={styles.buttonImg}
           src="/images/search.png"
--- src/pages/Skill/index.tsx
+++ src/pages/Skill/index.tsx
@@ -5,6 +5,22 @@ import SearchHeader from './components/search_header/search_header'
 import VideoList from './components/video_list/video_list'
 export default function Skill() {
   const [videos, setVideos] = useState([])
+  const search = (query: any) => {
+    const requestOptions: RequestInit = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_API_KEY}`,
+      requestOptions,
+    )
+      .then(response => response.json())
+      .then(result =>
+        result.items.map((item: any) => ({ ...item, id: item.id.videoId })),
+      )
+      .then(result => setVideos(result))
+      .catch(error => console.log('error', error))
+  }
 
   useEffect(() => {
     const requestOptions: RequestInit = {
@@ -16,14 +32,17 @@ export default function Skill() {
       requestOptions,
     )
       .then(response => response.json())
-      .then(result => setVideos(result.items))
+
+      .then(result => {
+        setVideos(result.items)
+      })
       .catch(error => console.log('error', error))
   }, [])
 
   return (
     <Container>
       <Heading heading="YouTube" />
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </Container>
   )
