6e23a18826cea10a68885f205dfafdb2bd5fb18e
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-10-01 21:05:24 +0900

    detail

--- src/pages/Skill/components/video_detail/video_detail.tsx
+++ src/pages/Skill/components/video_detail/video_detail.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import styles from './video_detail.module.css'
 export default function VideoDetail({ video }: any) {
-  return <h1>VideoDetail</h1>
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.video}
+        // type="text/html"
+        width="100%"
+        height="500px"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <h2>{video.snippet.title}</h2>
+      <h3>{video.snippet.channelTitle}</h3>
+      <pre>{video.snippet.description}</pre>
+    </section>
+  )
 }
