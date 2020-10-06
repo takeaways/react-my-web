d8f2b7b35affbe964e563266b6a48a4d8bf38c1a
JangGeonil <wkdrjsdlf2@gmail.com>
2020-10-06 20:27:41 +0900

    edit form

--- src/pages/Business/components/card_edit_form/card_edit_form.tsx
+++ src/pages/Business/components/card_edit_form/card_edit_form.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import styles from './card_edit_form.module.css'
 import Button from '../button/button'
+import ImageFileInput from '../image_file_input/image_file_input'
+
 function CardEditForm({ card }: any) {
   const {
     name,
@@ -12,19 +15,33 @@ function CardEditForm({ card }: any) {
     fileURL,
   } = card
 
-  const onSubmit = () => {}
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
   return (
-    <form>
-      <input type="text" name="name" value={name} />
-      <input type="text" name="company" value={company} />
-      <input type="text" name="title" value={title} />
-      <input type="text" name="email" value={email} />
-      <select name="theme" value={theme}>
+    <form className={styles.form}>
+      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        value={company}
+      />
+      <select className={styles.select} name="theme" value={theme}>
         <option value="light">light</option>
         <option value="dark">dark</option>
         <option value="colorful">colorful</option>
       </select>
-      <textarea name="message" value={message}></textarea>
+      <input className={styles.input} type="text" name="title" value={title} />
+      <input className={styles.input} type="text" name="email" value={email} />
+      <textarea
+        className={styles.textarea}
+        name="message"
+        value={message}
+      ></textarea>
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
       <Button name={'Delete'} onClick={onSubmit} />
     </form>
   )
---
+++ src/pages/Business/components/image_file_input/image_file_input.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import styles from './image_file_input.module.css'
+export default function ImageFileInput(props: any) {
+  return <button>Image</button>
+}
